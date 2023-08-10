27682567d55664e17dfac6b18125159a7c269adf
Edward Stuckey <observer21.public@gmail.com>
2023-08-10 07:25:23 -0400

    some minor additions
    updated flywheels.h

--- Firmware/DetlaffDev/DettyFW/Detty/src/program/flywheels.h
+++ Firmware/DetlaffDev/DettyFW/Detty/src/program/flywheels.h
@@ -31,9 +31,9 @@ class singleMotor
     //returns TRUE if flywheel has reached speed
     bool update();
 
-    //set and get the target speed of the flywheel
-    void set_target_speed();
-    void get_target_speed();
+    //set and get the target speed of the flywheel, 
+    void set_target_speed(int speed);
+    int get_target_speed();
 
     //ONLY works with bidirectional dshot.
     //Otherwise, it returns 0 if the delay hasn't run out yet, returns target_speed if it has
@@ -51,10 +51,20 @@ class singleMotor
     //motor settings
 
     //if we assign a pin number of -1, we essentialy enable a dummy motor
-    bool is_valid_motor = false;
-    bool has_been_attached = false;
-    int pin_num = -1;
-    unsigned int delay_millis = 0;
+
+    int current_speed = 0; //speed the motor is trying to reach
+    int last_speed = 0; //speed the motor is reaching from
+    bool reached_speed = false; //did the motor reach its target?
+    unsigned int delay_millis = 0; //how long until the motor is revved at speed
+    unsigned long current_millis = 0; //how much delay is left
+    unsigned long start_millis = 0; //when the command to change speed was issued
+
+
+
+    bool is_valid_motor = false; //pin number needs to be valid
+    bool has_been_attached = false; //prevent attach() from happening more than 1x
+    int pin_num = -1; //pin to attach to, invalid by default
+
     flywheel_type_t flywheel_type = FLYWHEEL_TYPE_NULL;
     dshot_mode_t dshot_mode = DSHOT_OFF;
     bidirectional_mode_t dshot_bidir = NO_BIDIRECTION;
