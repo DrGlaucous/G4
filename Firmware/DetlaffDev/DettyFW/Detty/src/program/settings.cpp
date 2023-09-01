e70717bdf2c53f41e24a9c32486aaa3b61a2f99f
Dr. Glaucous <49349519+DrGlaucous@users.noreply.github.com>
2023-09-01 11:41:25 -0400

    a few small changes to firmware
    mostly just some work on the settings (such that it is)

--- Firmware/DetlaffDev/DettyFW/Detty/src/program/settings.cpp
+++ Firmware/DetlaffDev/DettyFW/Detty/src/program/settings.cpp
@@ -1,7 +1,8 @@
 
 
+#include <DShotRMT.h>
 #include <Arduino.h>
-#include "configuration.h"
+//#include "configuration.h"
 
 
 typedef enum shoot_mode_e
@@ -10,7 +11,33 @@ typedef enum shoot_mode_e
     SHOOT_MODE_FULL_AUTO,
     SHOOT_MODE_SELECT_FIRE,
     SHOOT_MODE_MAX,
+
 }shoot_mode_t;
+
+
+//////////////FLYWHEELS//////////////
+typedef enum esc_type_e
+{
+    ESC_TYPE_NULL,
+    ESC_TYPE_BRUSHED,
+    ESC_TYPE_PWM,
+    ESC_TYPE_DSHOT,
+    ESC_TYPE_MAX,
+
+}esc_type_t;
+
+
+//the settings for a single configuration (selectable with the preset buttons)
+typedef struct live_settings_s
+{
+    
+    shoot_mode_t shoot_mode;
+
+    unsigned int cache_delay;
+    unsigned int thisOne;
+
+} live_settings_t;
+
 
 //all settings in the blaster (as set by configuration.h)
 typedef struct all_settings_s
@@ -31,16 +58,6 @@ typedef struct all_settings_s
 
 } all_settings_t;
 
-//the settings for a single configuration (selectable with the preset buttons)
-typedef struct live_settings_s
-{
-    
-    shoot_mode_t shoot_mode;
-
-    unsigned int cache_delay;
-    unsigned int 
-
-} live_settings_t;
 
 
 
